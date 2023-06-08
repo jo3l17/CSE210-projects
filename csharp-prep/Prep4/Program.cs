33513cb4a9aa9d942d33111b796996bcbe2dff0a
jo3l17 <joelvaldezangeles@gmail.com>
2023-06-07 22:35:00 -0500

    feat: Prep 4 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<int> numbers = new List<int>();
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+    int number;
+    do
     {
-        Console.WriteLine("Hello Prep4 World!");
+      Console.Write("Enter number: ");
+      string userInput = Console.ReadLine();
+      number = Convert.ToInt32(userInput);
+      if (number != 0)
+      {
+        numbers.Add(number);
+      }
     }
+    while (number != 0);
+
+    int sum = numbers.Sum();
+    double average = numbers.Average();
+    int maxNumber = numbers.Max();
+
+    Console.WriteLine($"The sum is: {sum}");
+    Console.WriteLine($"The average is: {average}");
+    Console.WriteLine($"The largest number is: {maxNumber}");
+
+    List<int> positiveNumbers = numbers.Where(x => x > 0).ToList();
+    int smallestPositiveNumber = positiveNumbers.Min();
+
+    Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+
+    List<int> sortedNumbers = numbers.OrderBy(x => x).ToList();
+
+    Console.WriteLine("The sorted list is:");
+    foreach (int num in sortedNumbers)
+    {
+      Console.WriteLine(num);
+    }
+  }
 }
